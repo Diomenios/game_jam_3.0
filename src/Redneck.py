1eb8ddeda01fc49f578c7f9cfca897d1c42cc112
Simon Arys <simon.arys@student.uclouvain.be>
2021-03-20 23:52:05 +0100

    modifying speed of redneck diagonal

--- src/Redneck.py
+++ src/Redneck.py
@@ -1,4 +1,5 @@
 import CONST
+import math
 from Supporter import Supporter
 
 class Redneck(Supporter):
@@ -8,12 +9,20 @@ class Redneck(Supporter):
 
     def update(self, player_x, player_y):
         """ Move the redneck """
+        x_diff = player_x - self.sprite.center_x
+        y_diff = player_y - self.sprite.center_y
+
+        angle = math.atan2(y_diff, x_diff)
+
+        change_x = math.cos(angle) * CONST.SUPPORTER_INIT_VEL * self.boost_speed
+        change_y = math.sin(angle) * CONST.SUPPORTER_INIT_VEL * self.boost_speed
+
         if player_x > self.sprite.center_x:
-            self.sprite.center_x += CONST.SUPPORTER_INIT_VEL * self.boost_speed
+            self.sprite.center_x += change_x
         elif player_x < self.sprite.center_x:
-            self.sprite.center_x -= CONST.SUPPORTER_INIT_VEL * self.boost_speed
+            self.sprite.center_x += change_x
 
         if player_y > self.sprite.center_y:
-            self.sprite.center_y += CONST.SUPPORTER_INIT_VEL * self.boost_speed
+            self.sprite.center_y += change_y
         elif player_y < self.sprite.center_y:
-            self.sprite.center_y -= CONST.SUPPORTER_INIT_VEL * self.boost_speed
+            self.sprite.center_y += change_y
